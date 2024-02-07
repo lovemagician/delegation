f478d47dd6c4eebad30e830dd55820aeeeac5e81
lovemagician <ddiddibboddiddibbo@gmail.com>
2024-02-07 14:33:11 +0900

    first commit

---
+++ client/js/part03.js
@@ -0,0 +1,67 @@
+const data = [
+  {
+    id: 1,
+    src: "visual1.jpg",
+    alt: "모던한 테이블과 화분의 조화를 표현한 공간",
+  },
+  {
+    id: 2,
+    src: "visual2.jpg",
+    alt: "강렬한 의자의 색상과 따뜻한 느낌의 공간",
+  },
+  {
+    id: 3,
+    src: "visual3.jpg",
+    alt: "호텔 라운지 느낌의 편안한 의자가 있는 공간",
+  },
+  {
+    id: 4,
+    src: "visual4.jpg",
+    alt: "물방을 모양의 독특한 디자인의 의자들을 나열한 공간",
+  },
+];
+
+const swiper = new Swiper(".swiper", {
+  autoplay: {
+    delay: 4000,
+  },
+  loop: true,
+  speed: 2000,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  parallax: true,
+  pagination: {
+    el: ".pagination",
+    clickable: true,
+    bulletClass: "bullet",
+    bulletActiveClass: "is-active",
+    renderBullet: function (index, className) {
+      return /* html */ `
+        <span class="${className}">
+          <img src="./assets/part01/${data[index].src}" alt="${data[index].alt}.jpg" />
+        </span>
+      `;
+    },
+    // type:'fraction'
+  },
+});
+
+const split = new SplitText("h3", { type: "chars" });
+
+// split.char => array
+
+swiper
+  .on("slideChangeTransitionStart", function () {
+    gsap.to(split.chars, {
+      yPercent: 120,
+      stagger: 0.05,
+    });
+  })
+  .on("slideChangeTransitionEnd", function () {
+    gsap.to(split.chars, {
+      yPercent: 0,
+      stagger: 0.05,
+    });
+  });
